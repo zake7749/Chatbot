5817c0b9f756d42f0ad37bef74610d16f3f84be2
F74026284 <zake7749@gmail.com>
2016-08-11 15:38:01 +0800

    add jieba tf/idf and textrank funcs into console options.

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import jieba
+import jieba.analyse
 
 import rulebase
 
@@ -19,24 +20,52 @@ def main():
         show_information()
         choice = input('Your choice is: ')
         choice = choice.lower()
-        if choice == 'p':
+        if choice == 'e':
+            res = jieba_tf_idf()
+            for tag, weight in res:
+                print('%s %s' % (tag, weight))
+        elif choice == 'g':
+            res = jieba_textrank()
+            for tag, weight in res:
+                print('%s %s' % (tag, weight))
+        elif choice == 'p':
             print(rb)
         elif choice == 'r':
             rb.load_rules('rule/baserule.txt')
-        else:
+        elif choice == 't':
             test_speech(rb,sw)
+        elif choice == 'm':
+            speech = input('Input a sentence:')
+            res,path = rule_match(rb,sw,speech)
+            write_output(speech,res,path)
+        elif choice == 'q':
+            exit()
+        else:
+            print('[X] ' + choice + '.')
+
+def jieba_textrank():
+    speech = input('Input a sentence: ')
+    return jieba.analyse.textrank(speech, withWeight=True, topK=20)
+
+def jieba_tf_idf():
+    speech = input('Input a sentence: ')
+    return jieba.analyse.extract_tags(speech, topK=20, withWeight=True)
 
 def show_information():
     print('Here is chat bot backend, echo your choice.')
     print('-------------------------------------------')
+    print('E)xtract the name entity.')
+    print('G)ive me the TextRank.')
+    print('M)atch sentence with rules.')
     print('P)rint the rule in the rulebase.')
     print('R)eload the baserule.txt.')
     print('T)est the data in speech.txt.')
+    print('Q)uit.')
     print('-------------------------------------------')
 
 def init_jieba():
     #jieba custom setting
-    #jieba.load_userdict("userdict.txt") TODO
+    #jieba.load_userdict("userdict.txt")
     jieba.set_dictionary('jieba_dict/dict.txt.big')
 
 def load_stopword():
@@ -48,29 +77,41 @@ def load_stopword():
             stopword.add(sw)
     return stopword
 
-def test_speech(rb,stopword):
+def rule_match(rb, stopword, sentence):
+
+    words = jieba.cut(sentence, HMM=False)
+    #clean up the stopword
+    keyword = []
+    for word in words:
+        if word not in stopword:
+            keyword.append(word)
+
+    return rb.match(keyword,threshold=0.1)
+
+def test_speech(rb, stopword):
 
     output = open('test/output.txt','w',encoding='utf-8')
     # load sample data
     with open('test/speech.txt','r',encoding='utf-8') as input:
         for speech in input:
             speech = speech.strip('\n')
-            sentence = jieba.cut(speech, HMM=False)
+            result,path = rule_match(rb, stopword, speech)
+            write_output(speech, result, path, output)
 
-            #clean up the stopword
-            keyword = []
-            for word in sentence:
-                if word not in stopword:
-                    keyword.append(word)
+def write_output(org_speech, result, path, output = None):
 
-            result,path = rb.match(keyword,threshold=0.1)
+    result_information = ''
+    result_information += "Case# " + str(org_speech) + '\n'
+    result_information += "------------------\n"
+    for similarity,rule,matchee in result:
+        str_sim = '%.4f' % similarity
+        result_information += str_sim+'\t'+path+rule+'\t\t'+matchee+'\n'
+    result_information += "------------------\n"
 
-            output.write("Case# " + str(speech) + '\n')
-            output.write("------------------\n")
-            for similarity,rule,matchee in result:
-                str_sim = '%.4f' % similarity
-                output.write(str_sim+'\t'+path+rule+'\t\t'+matchee+'\n')
-            output.write("------------------\n")
+    if output is None:
+        print(result_information)
+    else:
+        output.write(result_information)
 
 if __name__ == '__main__':
     main()
